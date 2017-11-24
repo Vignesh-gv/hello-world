d92f0630e114f267c85a1ecc00c9e6a2fd6d8618
Vignesh-gv <vigneshgovind135@gmail.com>
2017-11-24 14:31:01 +0530

    track
    Tracking

---
+++ ViewController+utls.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController+utls.swift
+//  eventTracking
+//
+//  Created by Vignesh on 23/11/17.
+//  Copyright © 2017 ThoughtMaQers. All rights reserved.
+//
+
+import UIKit
+
+private let swizzling: (UIViewController.Type) -> () = { viewController in
+    
+    let originalSelector = #selector(viewController.viewWillAppear(_:))
+    let swizzledSelector = #selector(viewController.proj_viewWillAppear(animated:))
+    
+    let originalMethod = class_getInstanceMethod(viewController, originalSelector)
+    let swizzledMethod = class_getInstanceMethod(viewController, swizzledSelector)
+    
+    method_exchangeImplementations(originalMethod!, swizzledMethod!) }
+
+extension UIViewController {
+    
+    static let classInit: Void = {
+        // make sure this isn't a subclass
+        swizzling(UIViewController.self)
+    }()
+    
+    // MARK: - Method Swizzling
+    
+    @objc func proj_viewWillAppear(animated: Bool) {
+        self.proj_viewWillAppear(animated: animated)
+        
+        let viewControllerName = NSStringFromClass(type(of: self))
+        update()
+        print("viewWillAppear: \(viewControllerName)")
+    }
+    
+    func update(){
+        print("update to server")
+    }
+}
---
+++ control+utls.swift
@@ -0,0 +1,30 @@
+//
+//  control+utls.swift
+//  eventTracking
+//
+//  Created by Vignesh on 23/11/17.
+//  Copyright © 2017 ThoughtMaQers. All rights reserved.
+//
+
+import UIKit
+
+private let swizzling: (AnyClass, Selector, Selector) -> () = { forClass, originalSelector, swizzledSelector in
+    let originalMethod = class_getInstanceMethod(forClass, originalSelector)
+    let swizzledMethod = class_getInstanceMethod(forClass, swizzledSelector)
+    method_exchangeImplementations(originalMethod!, swizzledMethod!)
+}
+
+extension UIControl {
+    
+    static let classCInit: Void = {
+        let originalSelector = #selector(sendAction(_:to:for:))
+        let swizzledSelector = #selector(swizzled_sendAction(_:to:for:))
+        swizzling(UIControl.self, originalSelector, swizzledSelector)
+    }()
+    
+    @objc func swizzled_sendAction(_ action: Selector, to target: Any?, for event: UIEvent?) {
+        swizzled_sendAction(action, to: target, for: event)
+        print("Method was triggered: \(action)")
+    }
+    
+}
